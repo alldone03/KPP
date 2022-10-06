5226e92edc870d10a91bb0aa86349e96601d5a4a
mudalin1212 <aldanarsenal@gmail.com>
2022-10-06 21:42:06 +0700

    error pada if else

--- 001.cpp
+++ 001.cpp
@@ -1,63 +1,82 @@
 // NAMA		: Aldan Prayogi
 // NRP		: 2040221034
 // Jurusan	: Teknik Elektro Otomasi
-
+#include <iomanip>
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <limits>
+
 using namespace std;
 
 #define GRAVITASI 10       // 10 m/s^2
 #define START_PENGUKURAN 1 // pengukuran dimulai dari 1 meter
 #define SUDUT 45           // sudut elevasi tembakan
 
-int speed_dgn_loss(int variabel1)
+double speed_dgn_loss(double data)
 {
     /* tulis fungsi hitung_loss kalian disini */
     int loses;
-    if (variabel1 >= 1 && variabel1 <= 10)
-    {
-        loses = 1;
-    }
-    else if (variabel1 >= 11 && variabel1 <= 20)
-    {
-        loses = 3;
-    }
-    else if (variabel1 >= 21 && variabel1 <= 30)
-    {
-        loses = 5;
-    }
-    else if (variabel1 > 30)
-    {
-        return 0;
-    }
+    loses = (data > 1 && data < 10) ? 1 : loses;
+    loses = (data > 10 && data < 20) ? 3 : loses;
+    loses = (data > 21 && data < 30) ? 5 : loses;
+    // if (data > 1 - 1 && data < 10)
+    // {
+    //     return 1;
+    // }
+    // else if (data > 11 - 1 && data < 20)
+    // {
+    //     return 3;
+    // }
+    // else if (data > 21 - 1 && data < 30)
+    // {
+    //     return 5;
+    // }
+    // else
+    // {
+    //     /* code */
+    // }
 
-    return variabel1 - loses;
+    // else if (variabel1 > 30)
+    // {
+    //     return 0;
+    // }
+    return data - loses;
 }
-unsigned int mencari_V0(int variabel1)
+
+double mencari_V0(float variabel1)
 {
     //, int variabel2
     // v1 = memasukan persamaan ketika kecepatan
     // v2 = waktu berapa
 
     /* Tulis fungsi mencari v0 kalian disini */
-    int v0;
+
     // jarak ketinggian
-    v0 = pow(speed_dgn_loss(variabel1), 2) * asin(2 * SUDUT) / GRAVITASI;
-    return v0;
+
+    return round(pow(speed_dgn_loss(variabel1), 2) * asin(2 * double(SUDUT)) / double(GRAVITASI));
 }
 
 int main()
 {
     /* tulis kode utama kalian disini */
+    float a = 3;
+    // cout << numeric_limits<double>::max();
+    // cout << sizeof(a);
 
-    int sudut_tembakan;
-    cout << "Sudut Tembakan :";
-    cin >> sudut_tembakan;
-    // string datahello = "Sudut Tembakan Adalah " + to_string(sudut_tembakan);
-    cout << "Sudut Tembakan Adalah " + to_string(mencari_V0(sudut_tembakan)) << endl;
+    double input = 10.0711;
+    // if (input > 10)
+    // {
+    //     printf("oke");
+    // }
+
+    printf("%s\n", speed_dgn_loss(input));
+
+    cout << "INPUT : \n";
+    // cin >> input;
+    // cout << speed_dgn_loss(input) << "\n";
+    cout << "Jarak Adalah : " + to_string(mencari_V0(input)) << endl;
 
     return 0;
-
     /* cout << jarak << " " << kecepatan_tangensial << endl */
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,3 @@
-int mencari_V0(int variabel1, int variabel2)
-// {
-//     /* Tulis fungsi mencari v0 kalian disini */
-
-//     return 0;
-// }
-
-// int speed_dgn_loss(int variabel1)
-// {
-//     /* tulis fungsi hitung_loss kalian disini */
-//     int loses;
-//     if (variabel1 >= 1 && variabel1 <= 10)
-//     {
-//         loses = 1;
-//     }
-//     else if (variabel1 >= 11 && variabel1 <= 20)
-//     {
-//         loses = 3;
-//     }
-//     else if (variabel1 >= 21 && variabel1 <= 5)
-//     {
-//         loses = 5;
-//     }
-
-//     float v0 = (2 * variabel1 / variabel2) ^ 0, 5;
-
-//     return 0;
-// }
+    // loses = (data > 1 && data < 10) ? 1 : loses;
+    // loses = (data > 10 && data < 20) ? 3 : loses;
+    // loses = (data > 21 && data < 30) ? 5 : loses;
