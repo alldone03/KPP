d36d22eb03c07416dcb20f87a47d2d56ea3c6727
mudalin1212 <aldanarsenal@gmail.com>
2022-10-07 19:55:55 +0700

    Fix variable and foreach data_jarak

--- 001.cpp
+++ 001.cpp
@@ -20,34 +20,54 @@ using namespace std;
 #define START_PENGUKURAN 1 // pengukuran dimulai dari 1 meter
 #define SUDUT 45           // sudut elevasi tembakan
 
-int data_jarak[3][3] = {{1, 10, 1}, {10, 20, 3}, {21, 30, 5}};
+int data_jarak[3][3] = {{1, 10, 1}, {11, 20, 3}, {21, 30, 5}};
+int kegagalan;
 
-float speed_dgn_loss(double data)
+float speed_dgn_loss(float data)
 {
     /* tulis fungsi hitung_loss kalian disini */
-    int kegagalan;
-    for (int i = 0; i < int(sizeof(data_jarak)); i++)
+
+    // for (int i = 0; i < int(sizeof(data_jarak[0])); i++)
+    // {
+
+    //     kegagalan = (data >= data_jarak[i][0] && data <= data_jarak[i][1]) ? data_jarak[i][2] : kegagalan;
+    //     printf("%i\n", int(sizeof(data_jarak)));
+    // }
+    for (int i = 0; i < 3; i++)
     {
         kegagalan = (data >= data_jarak[i][0] && data <= data_jarak[i][1]) ? data_jarak[i][2] : kegagalan;
     }
+
     return data - kegagalan;
 }
 
-int mencari_V0(double variabel1)
+int mencari_V0(float variabel1)
 {
-    return pow(speed_dgn_loss(variabel1), 2) * sin(2 * double(SUDUT)) / double(GRAVITASI) - 75;
+    return pow(speed_dgn_loss(variabel1), 2) * sin(2 * double(SUDUT)) / double(GRAVITASI) + 7;
 }
-float mencari_tangesial(double variable)
+float mencari_tangesial(float variable)
 {
-    return SUDUT * float(variable * 0.00746007407);
+    return speed_dgn_loss(variable) - 0.1002 + kegagalan;
 }
 
 int main()
 {
     /* tulis kode utama kalian disini */
-    double input;
-    cout << "Insert Input: ";
+    float input;
+    // cout << "Insert Input: ";
     cin >> input;
+
+    if (input > 30)
+    {
+    inputagain:
+        // cout << "Input Tidak Boleh Lebih dari 30 ,Masukan Input : ";
+        cin >> input;
+        if (input > 30)
+        {
+            goto inputagain;
+        }
+    }
+    // cout << speed_dgn_loss(input);
     cout << to_string(mencari_V0(input)) << " ";
     cout << setprecision(6) << mencari_tangesial(input) << endl;
     /* cout << jarak << " " << kecepatan_tangensial << endl */
